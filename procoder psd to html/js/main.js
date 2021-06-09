a7429ef11ada9617651aeefa7013af2c8db97312
rahat013 <75842312+rahat013@users.noreply.github.com>
2021-06-09 09:03:35 -0600

    Add files via upload

---
+++ procoder psd to html/js/main.js
@@ -0,0 +1,8 @@
+//slider js
+$(document).ready(function(){
+    $('.full-slider-area').bxSlider();
+    $('#main-menu-content').meanmenu({
+        meanMenuContainer: '#mobile-menu',
+        meanScreenWidth: 991,
+    });
+  });
